7c1694a6cbfba42194c60fbde8fcad2f1bb54bc6
G31W <gerardp.aries@gmail.com>
2025-05-30 01:07:33 -0600

    Ajustes de redirecciones
    Ajustes de redirecciones para Xbox y Microsoft Store

--- mssredirect.js
+++ mssredirect.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Microsoft Store EN-US to ES-MX Redirect
 // @namespace    https://apps.microsoft.com/
-// @version      1.0
+// @version      1.1
 // @description  Redirige automáticamente Microsoft Store de en-US a es-MX.
 // @author       Gerardo93
 // @match        https://apps.microsoft.com/detail/*
@@ -14,7 +14,7 @@
     'use strict';
     const url = new URL(window.location.href);
 
-    const needsRedirect = url.searchParams.get('hl') === 'en-US' || url.searchParams.get('gl') === 'US';
+    const needsRedirect = url.searchParams.get('hl') === 'en-US' || url.searchParams.get('hl') === 'en-us';
     if (needsRedirect) {
         url.searchParams.set('hl', 'es-MX');
         url.searchParams.set('gl', 'MX');
--- xboxredirect.js
+++ xboxredirect.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Xbox EN-US to ES-MX Redirect
 // @namespace    https://xbox.com/
-// @version      1.0
+// @version      1.1
 // @description  Redirige automáticamente URLs de Xbox en-US a es-MX en la tienda de juegos.
 // @author       Gerardo93
 // @match        https://www.xbox.com/en-us/games/store/*
+// @match        https://www.xbox.com/en-US/games/store/*
 // @downloadURL  https://github.com/Gerardo93/scriptsjs/raw/main/xboxredirect.js
 // @updateURL    https://github.com/Gerardo93/scriptsjs/raw/main/xboxredirect.js
 // @grant        none
@@ -12,8 +13,11 @@
 
 (function () {
     'use strict';
-    const newUrl = window.location.href.replace('/en-us/', '/es-mx/');
-    if (window.location.href !== newUrl) {
-        window.location.replace(newUrl);
+    const currentUrl = window.location.href;
+    if (/\/en-us\//i.test(currentUrl)) {
+        const newUrl = currentUrl.replace(/\/en-us\//i, '/es-mx/');
+        if (window.location.href !== newUrl) {
+            window.location.replace(newUrl);
+        }
     }
 })();
