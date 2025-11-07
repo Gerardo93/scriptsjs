d79bcfdf55482bc8412654e71c0f26998fd6af13
G31W <gerardp.aries@gmail.com>
2025-11-07 15:06:27 -0600

    Actualizar versión del script a 1.1
    Actualizar versión del script a 1.1 y mejorar la inserción del botón de SteamDB en páginas de aplicaciones.

--- steam2steamdb.js
+++ steam2steamdb.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam to SteamDB Button
 // @namespace    https://store.steampowered.com/
-// @version      1.0
+// @version      1.1
 // @description  Adds a SteamDB button to Steam app, bundle, or sub pages in the appropriate action container.
 // @author       Gerardo93
 // @match        https://store.steampowered.com/app/*
@@ -25,12 +25,21 @@
 
     const button = document.createElement('button');
     button.className = 'btnv6_blue_hoverfade btn_medium';
-    button.style.marginTop = '16px';
     button.innerHTML = '<span>View on SteamDB</span>';
     button.addEventListener('click', () => window.open(steamDbUrl, '_blank'));
 
     const containerId = type === 'app' ? 'queueActionsCtn' : 'game_area_purchase_top';
     const container = document.getElementById(containerId);
 
-    if (container) container.appendChild(button);
+    if (container && type === 'app') {
+        const new_container = container.cloneNode(true);
+        new_container.id = 'steamdbButtonContainer';
+        new_container.innerHTML = '';
+        new_container.style.paddingTop = 'unset';
+        container.parentNode.insertBefore(new_container, container.nextSibling);
+        new_container.appendChild(button);
+    }
+    else if (container) {
+        container.appendChild(button);
+    }
 })();
